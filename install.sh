f79ea12e2e227722c4b73dcc52ae330eaf0289f0
Gustavo <gustavo_guga_01@hotmail.com>
2024-08-31 11:43:50 -0300

    rms python dependencies

--- install.sh
+++ install.sh
@@ -42,17 +42,3 @@ cd client-node
 cd ..
 npm i
 echo -e "${OK_EMOJI_TEXT}"
-
-if [ -n "$(command -v python3 --version)" ]; then
-  echo -e "${YELLOW}Installing Python 3...${RESET}"
-  sudo apt install python3
-  python_version=$(python3 --version)
-  echo -e "${GREEN}Installed version: ${RESET}${RED}${python_version}${RESET}"
-  echo -e "${OK_EMOJI_TEXT}"
-fi
-
-echo -e "${YELLOW}Installing Python dependencies...${RESET}"
-cd client-python
-pip install -r requirements.txt
-cd ..
-echo -e "${OK_EMOJI_TEXT}"
